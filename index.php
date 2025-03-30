a3f983290a0e1cba2b12bf51eed069caa521b588
Sumit Rathor <sumitrathor142272@gmail.com>
2025-03-30 22:23:18 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,474 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>Sachiva</title>
+
+    <!-- slider stylesheet -->
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <!-- font awesome style -->
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="css/responsive.css" rel="stylesheet" />
+    <style>
+    .main-logo {
+        width: 60px;
+    }
+
+    .feature_section {
+        position: relative;
+        top: 60px;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="hero_area">
+        <!-- header section strats -->
+        <header class="header_section">
+            <div class="header_top">
+                <div class="container-fluid">
+                    <div class="contact_nav">
+                        <a href="tel:+918818848558">
+                            <i class="fa fa-phone" aria-hidden="true"></i>
+                            <span>
+                                Call : +91 8818848558
+                            </span>
+                        </a>
+                        <a href="mailto:websecurtiysachiva@gmail.com">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                            <span>
+                                Email : websecurtiysachiva@gmail.com
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="header_bottom">
+                <div class="container-fluid">
+                    <nav class="navbar navbar-expand-lg custom_nav-container ">
+                        <a class="navbar-brand" href="./">
+                            <img src="images/logo_blue.png" alt="Logo" class="main-logo">
+                            <span>
+                                sachiva
+                            </span>
+                        </a>
+
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class=""> </span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="./">Home <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="about.html"> About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="service.html">Services</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.html">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <!-- end header section -->
+        <!-- slider section -->
+        <section class="slider_section ">
+            <div class="container ">
+                <div class="row">
+                    <div class="col-md-6 ">
+                        <div class="detail-box">
+                            <h1>
+                                Repair and <br>
+                                Maintenance <br>
+                                Services
+                            </h1>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui harum voluptatem adipisci.
+                                Quos molestiae saepe dicta nobis pariatur, tempora iusto, ad possimus soluta hic
+                                praesentium mollitia consequatur beatae, aspernatur culpa.
+                            </p>
+                            <a href="">
+                                Contact Us
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="img-box">
+                            <img src="images/maintenance.gif" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- end slider section -->
+    </div>
+
+    <!-- end feature section -->
+
+    <!-- about section -->
+
+    <section class="about_section layout_padding-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-md-6">
+                    <div class="detail-box">
+                        <h2>
+                            About us
+                        </h2>
+                        <p>
+                            There are many variations of passages of Lorem Ipsum available, but the majority have
+                            suffered alteration in some form, by injected humour, or randomisedThere are many variations
+                            of passages of Lorem Ipsum available, but the majority have suffered alteration in some
+                            form, by injected humour, or randomised
+                        </p>
+                        <a href="">
+                            Read More
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-7 col-md-6">
+                    <div class="img-box">
+                        <img src="images/about-us.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end about section -->
+
+
+    <!-- professional section -->
+
+    <section class="professional_section layout_padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="img-box">
+                        <img src="images/professional-img.png" alt="">
+                    </div>
+                </div>
+                <div class="col-md-6 ">
+                    <div class="detail-box">
+                        <h2>
+                            We Provide Professional <br>
+                            Home Services.
+                        </h2>
+                        <p>
+                            randomised words which don't look even slightly believable. If you are going to use a
+                            passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the
+                            middle of text. All randomised words which don't look even slightly
+                        </p>
+                        <a href="">
+                            Read More
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end professional section -->
+
+    <!-- service section -->
+
+    <section class="service_section layout_padding">
+        <div class="container ">
+            <div class="heading_container heading_center">
+                <h2> Our Services </h2>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-md-3 mx-auto">
+                    <div class="box ">
+                        <div class="img-box">
+                            <img src="images/s1.png" alt="" />
+                        </div>
+                        <div class="detail-box">
+                            <h5>
+                                Web Development
+                            </h5>
+                            <p>
+                                when looking at its layout. The point of using Lorem Ipsum is
+                                that it has a more-or-less normal
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 mx-auto">
+                    <div class="box ">
+                        <div class="img-box">
+                            <img src="images/s1.png" alt="" />
+                        </div>
+                        <div class="detail-box">
+                            <h5>
+                                Software Management
+                            </h5>
+                            <p>
+                                when looking at its layout. The point of using Lorem Ipsum is
+                                that it has a more-or-less normal
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 mx-auto">
+                    <div class="box ">
+                        <div class="img-box">
+                            <img src="images/s2.png" alt="" />
+                        </div>
+                        <div class="detail-box">
+                            <h5>
+                                Google Ads
+                            </h5>
+                            <p>
+                                when looking at its layout. The point of using Lorem Ipsum is
+                                that it has a more-or-less normal
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 mx-auto">
+                    <div class="box ">
+                        <div class="img-box">
+                            <img src="images/s3.png" alt="" />
+                        </div>
+                        <div class="detail-box">
+                            <h5>
+                                Cybersecurity
+                            </h5>
+                            <p>
+                                when looking at its layout. The point of using Lorem Ipsum is
+                                that it has a more-or-less normal
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="btn-box">
+                <a href="">
+                    View More
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <!-- end service section -->
+
+    <!-- client section -->
+
+    <section class="client_section ">
+        <div class="container">
+            <div class="heading_container heading_center">
+                <h2>
+                    What Our Clients Say
+                </h2>
+            </div>
+            <div class="carousel-wrap layout_padding2-top ">
+                <div class="owl-carousel">
+                    <div class="item">
+                        <div class="box">
+                            <div class="client_id">
+                                <div class="img-box">
+                                    <img src="images/client-1.jpg" alt="">
+                                </div>
+                                <div class="client_detail">
+                                    <div class="client_info">
+                                        <h6>
+                                            Jorch morik
+                                        </h6>
+                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                        <i class="fa fa-star" aria-hidden="true"></i>
+                                    </div>
+                                    <i class="fa fa-quote-left" aria-hidden="true"></i>
+                                </div>
+                            </div>
+                            <div class="client_text">
+                                <p>
+                                    chunks as necessary, making this the first true generator on the Internet. It uses a
+                                    dictionary of over 200 Latin words, combined with a handful of model sentence
+                                    structures, to generate Lorem Ipsum
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end client section -->
+
+    <!-- contact section -->
+
+    <section class="contact_section layout_padding">
+        <div class="container">
+            <div class="heading_container">
+                <h2>
+                    Contact Us
+                </h2>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <form action="">
+                        <div>
+                            <input type="text" placeholder="Name" />
+                        </div>
+                        <div>
+                            <input type="text" placeholder="Phone Number" />
+                        </div>
+                        <div>
+                            <input type="email" placeholder="Email" />
+                        </div>
+                        <div>
+                            <input type="text" class="message-box" placeholder="Message" />
+                        </div>
+                        <div class="d-flex ">
+                            <button>
+                                SEND
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                    <div class="map_container">
+                        <div class="map">
+                            <div id="googleMap" style="width:100%;height:100%;"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- end contact section -->
+
+
+    <!-- info section -->
+    <section class="info_section ">
+        <div class="container">
+            <h4>
+                Get In Touch
+            </h4>
+            <div class="row">
+                <div class="col-lg-10 mx-auto">
+                    <div class="info_items">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <a href="">
+                                    <div class="item ">
+                                        <div class="img-box ">
+                                            <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                        </div>
+                                        <p>
+                                            Lorem Ipsum is simply dummy text
+                                        </p>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-4">
+                                <a href="">
+                                    <div class="item ">
+                                        <div class="img-box ">
+                                            <i class="fa fa-phone" aria-hidden="true"></i>
+                                        </div>
+                                        <p>
+                                            +02 1234567890
+                                        </p>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-md-4">
+                                <a href="">
+                                    <div class="item ">
+                                        <div class="img-box">
+                                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                                        </div>
+                                        <p>
+                                            demo@gmail.com
+                                        </p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="social-box">
+            <h4>
+                Follow Us
+            </h4>
+            <div class="box">
+                <a href="">
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+                <a href="">
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                </a>
+                <a href="">
+                    <i class="fa fa-youtube" aria-hidden="true"></i>
+                </a>
+                <a href="">
+                    <i class="fa fa-instagram" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+
+
+
+    <!-- end info_section -->
+
+    <!-- footer section -->
+    <footer class="footer_section">
+        <div class="container">
+            <p>
+                &copy; <span id="displayDateYear"></span> All Rights Reserved By
+                <a href="https://html.design/">Free Html Templates</a>
+            </p>
+        </div>
+    </footer>
+    <!-- footer section -->
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+    </script>
+    <script src="js/custom.js"></script>
+    <!-- Google Map -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
+    </script>
+    <!-- End Google Map -->
+
+
+</body>
+
+</html>
